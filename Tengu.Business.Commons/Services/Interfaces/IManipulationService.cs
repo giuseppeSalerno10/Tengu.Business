09496283fca53d1404c30f56050ced21f624d523
Giuseppe Salerno <GiuseppeSalerno@outlook.it>
2022-06-21 21:39:17 +0200

    Refactor

---
+++ Tengu.Business.Commons/Services/Interfaces/IManipulationService.cs
@@ -0,0 +1,11 @@
+﻿using Tengu.Business.Commons.Objects;
+
+namespace Tengu.Business.Commons.Services.Interfaces
+{
+    public interface IManipulationService
+    {
+        void HandleTenguException<TModel>(Exception e, ref TenguResult<TModel> result);
+        void HandleTenguException(Exception e, ref TenguResult result);
+
+    }
+}
---
+++ Tengu.Business.Commons/Services/ManipulationService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tengu.Business.Commons.Objects;
+using Tengu.Business.Commons.Services.Interfaces;
+
+namespace Tengu.Business.Commons.Services
+{
+    public class ManipulationService : IManipulationService
+    {
+        public void HandleTenguException<TModel>(Exception e, ref TenguResult<TModel> result)
+        {
+            HandleTenguException(e, ref result);
+        }
+
+        public void HandleTenguException(Exception e, ref TenguResult result)
+        {
+            switch (e)
+            {
+                case TenguException tenguException:
+                    result.Exception = tenguException;
+                    break;
+
+                case Exception exception:
+                    result.Exception = new TenguException(exception.Message);
+                    break;
+            }
+
+            result.Success = false;
+        }
+    }
+}
