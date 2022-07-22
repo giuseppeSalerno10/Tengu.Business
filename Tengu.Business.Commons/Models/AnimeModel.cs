d25ab762379347bbfa57c266bf4814f2775e0568
Giuseppe Salerno <GiuseppeSalerno@outlook.it>
2022-07-22 15:21:02 +0200

    normalize string

--- Tengu.Business.Commons/Models/AnimeModel.cs
+++ Tengu.Business.Commons/Models/AnimeModel.cs
@@ -4,12 +4,13 @@ namespace Tengu.Business.Commons.Models
 {
     public class AnimeModel
     {
+        private string title = string.Empty;
+
         public string Id { get; set; } = string.Empty;
         public TenguHosts Host { get; set; } = TenguHosts.None;
         public string Url { get; set; } = string.Empty;
         public string Image { get; set; } = string.Empty;
-        public string Title { get; set; } = string.Empty;
-
+        public string Title { get => title; set => title = value.Normalize(); }
         public KitsuAnimeModel? KitsuAttributes { get; set; }
 
     }
--- Tengu.Business.Commons/Models/EpisodeModel.cs
+++ Tengu.Business.Commons/Models/EpisodeModel.cs
@@ -4,10 +4,12 @@ namespace Tengu.Business.Commons.Models
 {
     public class EpisodeModel : IComparable<EpisodeModel>
     {
+        private string title = string.Empty;
+
         public string Id { get; set; } = string.Empty;
         public string AnimeId { get; set; } = string.Empty;
         public TenguHosts Host { get; set; } = TenguHosts.None;
-        public string Title { get; set; } = string.Empty;
+        public string Title { get => title; set => title = value.Normalize(); }
         public string Url { get; set; } = string.Empty;
         public string Image { get; set; } = string.Empty;
         public string EpisodeNumber { get; set; } = string.Empty;
